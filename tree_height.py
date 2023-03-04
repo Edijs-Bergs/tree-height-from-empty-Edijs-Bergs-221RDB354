c90b07d5cb91e6e8df30d0d85ee5d7ba81bd3758
Edijs-Bergs-221RDB354 <124983369+Edijs-Bergs-221RDB354@users.noreply.github.com>
2023-03-04 20:48:10 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,52 +1,66 @@
+# python3
+
 import sys
 import threading
 import numpy
 
-
+def compute_possible_heights(i, parents, possible_heights):
+    i = int(i)
+    
+    if parents[i] == -1:
+        return 1
+ 
+    if not (possible_heights[i] == 0):
+        return possible_heights[i]
+ 
+    return (1 + compute_possible_heights(parents[i], parents, possible_heights))
+ 
 def compute_height(n, parents):
-    # Initialize an array to keep track of the heights of each node
-    heights = [0] * n
     max_height = 0
+    possible_heights = [0 for i in range(n)]
 
-    # Traverse the tree from each leaf to the root and update the heights array
     for i in range(n):
-        node = i
-        height = 0
-        while node != -1:
-            # If we've already computed the height of this node, use that value
-            if heights[node] != 0:
-                height += heights[node]
-                break
-            height += 1
-            node = parents[node]
-        heights[i] = height
-        max_height = max(max_height, height)
-
+        possible_heights[i] = compute_possible_heights(i, parents, possible_heights)
+        max_height = max(max_height, possible_heights[i])
+ 
     return max_height
 
-
 def main():
-    # Read input from file in the "tests" folder
-    file_name = input()
-    while 'a' in file_name:
-        print("File name cannot contain letter 'a'.")
+    command = input()
+    if "F" in command:
         file_name = input()
-
-    try:
-        with open(f"test/{file_name}", "r") as f:
-            n = int(f.readline().strip())
-            parents = list(map(int, f.readline().strip().split()))
-    except:
-        print("Error reading file.")
-        return
-
-    # Call the compute_height function and print the result
-    print(compute_height(n, parents))
-
+        path = "test/" + file_name
+        if not "a" in file_name:
+            contents = open(path, "r")
+            text = contents.read()
+            contents.close()
+            partitioned = text.partition("\n")
+            n = int(partitioned[0])
+            arr = partitioned[2].split(" ")
+            arr = numpy.array(arr)
+            print(compute_height(n, arr))
+        
+        
+    elif "I" in command:
+        n = int(input())
+        parents = input()
+        arr = parents.split(" ")
+        arr = numpy.array(arr)
+        print(compute_height(n, arr))
+        
+    
+    # let user input file name to use, don't allow file names with letter a
+    # account for github input inprecision
+    
+    # input number of elements
+    # input values in one variable, separate with space, split these values in an array
+    # call the function and output it's result
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+# threading.Thread(target=main).start()
+main()
+# print(numpy.array([1,2,3]))
