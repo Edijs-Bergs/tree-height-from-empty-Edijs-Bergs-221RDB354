7ef0fa82cf329aeff139c29f4e7d3df7a326acc2
Edijs-Bergs-221RDB354 <124983369+Edijs-Bergs-221RDB354@users.noreply.github.com>
2023-03-04 19:22:04 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,25 +1,49 @@
 import sys
 import threading
 import numpy
-
+import requests
 
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    # Create a list to store the height of each node
+    heights = [0] * n
+
+    # Calculate the height of each node by traversing the tree from bottom to top
+    for i in range(n):
+        if heights[i] == 0:
+            height = 1
+            j = parents[i]
+            while j != -1:
+                if heights[j] != 0:
+                    height += heights[j]
+                    break
+                height += 1
+                j = parents[j]
+            heights[i] = height
+
+    # Return the maximum height of the tree
+    return max(heights)
 
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    # Ask user to input the file name
+    file_name = input("Enter the file name: ")
+
+    # Read the input file from the Github repository
+    url = "https://raw.githubusercontent.com//DA-testa/tree-height-from-empty-Edijs-Bergs-221RDB354/main/tests/" + file_name
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the input values
+        values = response.text.split()
+        n = int(values[0])
+        parents = [int(x) for x in values[1:]]
+
+        # Call the compute_height function and print the result
+        print(compute_height(n, parents))
+    else:
+        print("Error: Unable to read file from Github.")
+
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
@@ -27,5 +51,3 @@ def main():
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
