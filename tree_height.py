dff434e9034c948f9f55f0067782d276f04a442d
Edijs-Bergs-221RDB354 <124983369+Edijs-Bergs-221RDB354@users.noreply.github.com>
2023-03-04 19:28:40 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,48 +1,47 @@
 import sys
 import threading
 import numpy
-import requests
+
 
 def compute_height(n, parents):
-    # Create a list to store the height of each node
+    # Initialize an array to keep track of the heights of each node
     heights = [0] * n
+    max_height = 0
 
-    # Calculate the height of each node by traversing the tree from bottom to top
+    # Traverse the tree from each leaf to the root and update the heights array
     for i in range(n):
-        if heights[i] == 0:
-            height = 1
-            j = parents[i]
-            while j != -1:
-                if heights[j] != 0:
-                    height += heights[j]
-                    break
-                height += 1
-                j = parents[j]
-            heights[i] = height
+        node = i
+        height = 0
+        while node != -1:
+            # If we've already computed the height of this node, use that value
+            if heights[node] != 0:
+                height += heights[node]
+                break
+            height += 1
+            node = parents[node]
+        heights[i] = height
+        max_height = max(max_height, height)
 
-    # Return the maximum height of the tree
-    return max(heights)
+    return max_height
 
 
 def main():
-    # Ask user to input the file name
-    file_name = input("Enter the file name: ")
+    # Read input from file in the "tests" folder
+    file_name = input("Enter file name: ")
+    while 'a' in file_name:
+        print("File name cannot contain letter 'a'.")
+        file_name = input("Enter file name: ")
 
-    # Read the input file from the Github repository
-    url = "https://raw.githubusercontent.com/DA-testa/tree-height-from-empty-Edijs-Bergs-221RDB354/tree/main/test" + file_name
-    response = requests.get(url)
+    try:
+        with open(f"tests/{file_name}", "r") as f:
+            n = int(f.readline().strip())
+            parents = list(map(int, f.readline().strip().split()))
+    except:
+        print("Error reading file.")
+        return
 
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Parse the input values
-        values = response.text.split()
-        n = int(values[0])
-        parents = [int(x) for x in values[1:]]
-
-        # Call the compute_height function and print the result
-        print(compute_height(n, parents))
-    else:
-        print("Error: Unable to read file from Github.")
+    # Call the compute_height function and print the result
+    print(compute_height(n, parents))
 
 
 # In Python, the default limit on recursion depth is rather low,
